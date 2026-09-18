e08f523e4afc8e91ba752b788aefeb6262b18095
meelie24 <meelie1997@gmail.com>
2026-09-18 07:43:31 -0400

    feat: add working Spin Out Plus history dashboard

---
+++ app/plus/page.tsx
@@ -0,0 +1,3 @@
+import { PlusDashboard } from '@/components/PlusDashboard';
+export const metadata = { title: 'Spin Out+ | Spin Out' };
+export default function PlusPage() { return <PlusDashboard />; }
